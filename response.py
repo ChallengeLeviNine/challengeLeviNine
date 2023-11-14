a4ec96a0795c7f0342f3d69eb10553a3f9e3ef62
0101dusica <trbovicdusica@gmail.com>
2023-11-14 20:16:49 +0100

    logic for advanced stats changed

--- response.py
+++ response.py
@@ -4,12 +4,17 @@ from flask import abort
 def generate_response(data, player_name):
     #all variables is set to zero
     games_played = ftm = fta = pm2 = pa2 = pm3 = pa3 = reb = blk = ast = stl = tov = 0
+    val = []
+    efg = []
+    shp = []
+    har = []
 
     #going through csv data, calcuating simple stats
     for player_stats in data:
         #checking for name from path
         if player_stats[0] == player_name:
             games_played += 1
+            #sum simple stats
             ftm += int(player_stats[2])
             fta += int(player_stats[3])
             pm2 += int(player_stats[4])
@@ -21,6 +26,11 @@ def generate_response(data, player_name):
             ast += int(player_stats[10])
             stl += int(player_stats[11])
             tov += int(player_stats[12])
+            #calculating advanced stats
+            val.append(round((int(player_stats[2])+2*int(player_stats[4])+3*int(player_stats[6])+int(player_stats[8])+int(player_stats[9])+int(player_stats[10])+int(player_stats[11]))-(int(player_stats[3])-int(player_stats[2])+int(player_stats[5])-int(player_stats[4])+int(player_stats[7])-int(player_stats[6])+int(player_stats[12])),2))
+            efg.append(round((int(player_stats[4])+int(player_stats[6])+0.5*int(player_stats[6]))/(int(player_stats[5])+int(player_stats[7]))*100,2))
+            shp.append(round((int(player_stats[2])+2*int(player_stats[4])+3*int(player_stats[6]))/(2*(int(player_stats[5])+int(player_stats[7])+0.475*int(player_stats[3])))*100,2))
+            har.append(round(int(player_stats[10])/(int(player_stats[5])+int(player_stats[7])+0.475*int(player_stats[3])+int(player_stats[10])+int(player_stats[12]))*100,2))
 
     #name doesn't exist
     if(games_played == 0):
@@ -32,32 +42,32 @@ def generate_response(data, player_name):
         'gamesPlayed': games_played,
         'traditional': {
             'freeThrows': {
-                'attempts' : fta,
-                'made' : ftm,
+                'attempts' : round(fta/games_played,1),
+                'made' : round(ftm/games_played,1),
                 'shootingPercentage' : round(ftm/fta*100,1),
             },
             'twoPoints' : {
-                'attempts' : pa2,
-                'made' : pm2,
+                'attempts' : round(pa2/games_played,1),
+                'made' : round(pm2/games_played,1),
                 'shootingPercentage' : round(pm2/pa2*100,1),
             },
             'threePoints' : {
-                'attempts' : pa3,
-                'made' : pm3,
+                'attempts' : round(pa3/games_played,1),
+                'made' : round(pm3/games_played,1),
                 'shootingPercentage' : round(pm3/pa3*100,1),
             },
-            'points' : round(ftm+2*pm2+3*pm3,1),
-            'rebounds' : reb,
-            'blocks' : blk,
-            'assists' : ast,
-            'steals' : stl,
-            'turnovers' : tov,
+            'points' : round((ftm+2*pm2+3*pm3)/games_played,1),
+            'rebounds' : round(reb/games_played,1),
+            'blocks' : round(blk/games_played,1),
+            'assists' : round(ast/games_played,1),
+            'steals' : round(stl/games_played,1),
+            'turnovers' : round(tov/games_played,1),
         },
         'advanced' : {
-            'valorization' : round((ftm+2*pm2+3*pm3+reb+blk+ast+stl)-(fta-ftm+pa2-pm2+pa3-pm3+tov),1),
-            'effectiveFieldGoalPercentage' : round((pm2+pm3+0.5*pm3)/(pa2+pa3)*100,1),
-            'trueShootingPercentage' : round((ftm+2*pm2+3*pm3)/(2*(pa2+pa3+0.475*fta))*100,1),
-            'hollingerAssistRatio' : round(ast/(pa2+pa3+0.475*fta+ast+tov)*100,1)
+            'valorization' : round(sum(val)/games_played,1),
+            'effectiveFieldGoalPercentage' : round(sum(efg)/games_played,1),
+            'trueShootingPercentage' : round(sum(shp)/games_played,1),
+            'hollingerAssistRatio' : round(sum(har)/games_played,1)
         }
     }
     
