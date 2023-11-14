0c14b43b85ea97c4c8733ebe60ea54865479e93d
0101dusica <trbovicdusica@gmail.com>
2023-11-14 13:14:27 +0100

    simple statistics loaded

--- statistics.py
+++ statistics.py
@@ -2,10 +2,24 @@ from flask import jsonify, abort
 
 # calculating statistics for API endpoint
 def statistics(data, player_name):
-    games_played = 0
+    games_played = ftm = fta = pm2 = pa2 = pm3 = pa3 = reb = blk = ast = stl = tov = 0
+
     for player_stats in data:
         if player_stats[0] == player_name:
             games_played += 1
+            ftm += int(player_stats[2])
+            fta += int(player_stats[3])
+            pm2 += int(player_stats[4])
+            pa2 += int(player_stats[5])
+            pm3 += int(player_stats[6])
+            pa3 += int(player_stats[7])
+            reb += int(player_stats[8])
+            blk += int(player_stats[9])
+            ast += int(player_stats[10])
+            stl += int(player_stats[11])
+            tov += int(player_stats[12])
+
+
     if(games_played == 0):
         abort(404, description=f"Player not found!")
 
@@ -14,26 +28,26 @@ def statistics(data, player_name):
         'gamesPlayed': games_played,
         'traditional': {
             'freeThrows': {
-                'attempts' : None,
-                'made' : None,
+                'attempts' : fta,
+                'made' : ftm,
                 'shootingPercentage' : None,
             },
             'twoPoints' : {
-                'attempts' : None,
-                'made' : None,
+                'attempts' : pa2,
+                'made' : pm2,
                 'shootingPercentage' : None,
             },
             'threePoints' : {
-                'attempts' : None,
-                'made' : None,
+                'attempts' : pa3,
+                'made' : pm3,
                 'shootingPercentage' : None,
             },
             'points' : None,
-            'rebounds' : None,
-            'blocks' : None,
-            'assists' : None,
-            'steals' : None,
-            'turnovers' : None,
+            'rebounds' : reb,
+            'blocks' : blk,
+            'assists' : ast,
+            'steals' : stl,
+            'turnovers' : tov,
         },
         'advanced' : {
             'valorization' : None,
